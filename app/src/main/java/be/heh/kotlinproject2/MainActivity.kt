eaa8f5d7b4d9174b30abf25a760e2ec6636700bf
KouptchN <kouptchinskyng@gmail.com>
2023-11-14 10:43:58 +0100

    gg

--- app/src/main/java/be/heh/kotlinproject2/MainActivity.kt
+++ app/src/main/java/be/heh/kotlinproject2/MainActivity.kt
@@ -1,11 +1,40 @@
 package be.heh.kotlinproject2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
+    lateinit var monThread : ThreadTest
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun xmlClicEvent(v:View){
+        when(v.id){
+            bt_main_txtchange.id->bpModifTexte()
+            bt_main_startTH.id->bpThreadGo()
+        }
+    }
+    private fun bpModifTexte(){
+        if(tv_main_texte.getText().equals("Hello World !")) tv_main_texte.setText("Hey Android !")
+        else tv_main_texte.setText("Hello World !")
+    }
+    private fun bpThreadGo() {
+        if (bt_main_startTH.getText().equals("Thread GO !")) {
+            monThread = ThreadTest(pb_main_progressionTH)
+            monThread.Go()
+            bt_main_startTH.setText("Thread Stop !")
+            Toast.makeText(getApplicationContext(), "Activation du Thread", Toast.LENGTH_LONG)
+                .show()
+        }
+        else {
+            monThread.Stop()
+            Toast.makeText(getApplicationContext(), "Désactivation du Thread", Toast.LENGTH_LONG)
+                .show()
+            bt_main_startTH.setText("Thread GO !")
+        }
     }
 }
--- app/src/main/java/be/heh/kotlinproject2/ThreadTest.kt
+++ app/src/main/java/be/heh/kotlinproject2/ThreadTest.kt
@@ -1,4 +1,35 @@
 package be.heh.kotlinproject2
 
+import android.widget.ProgressBar
+import java.util.concurrent.atomic.AtomicBoolean
+
 class ThreadTest {
+    private var pb_main_progressionTH: ProgressBar? = null
+    private var is_Running: AtomicBoolean = AtomicBoolean(false)
+    private lateinit var monThread: Thread
+    constructor(pb: ProgressBar) {
+        pb_main_progressionTH = pb
+    }
+    fun Go() {
+        pb_main_progressionTH!!.progress = 0
+        monThread = Thread(Runnable(){
+            var i = 0
+            while (i < 20 && is_Running.get()) {
+                pb_main_progressionTH!!.incrementProgressBy(5)
+                try {
+                    Thread.sleep(1000)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+                i++
+            }
+        })
+        is_Running.set(true)
+        monThread!!.start()
+    }
+    fun Stop(){
+        is_Running.set(false);
+        monThread!!.interrupt();
+        pb_main_progressionTH!!.setProgress(0);
+    }
 }
